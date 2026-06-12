2276f8da953d7f3fea3008b041dbb00354951bb3
Shivani <shivanikokkula527@gmail.com>
2026-06-12 19:33:25 +0530

    feat: maintain aspect ratio while resizing images

--- Project-2/image-processing/resize_image.py
+++ Project-2/image-processing/resize_image.py
@@ -3,9 +3,9 @@ from PIL import Image
 def resize_image(input_path, output_path):
     img = Image.open(input_path)
 
-    resized_img = img.resize((800, 600))
+    img.thumbnail((800, 600))
 
-    resized_img.save(output_path)
+    img.save(output_path)
 
     print("Image resized successfully!")
 
