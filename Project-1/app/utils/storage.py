6df59a7615d5dd980837f26189ab8a48049bfec0
sreejani <sreejaniminni@gmail.com>
2026-06-19 19:50:42 +0530

    Added original and processed image storage structure

--- Project-1/app/utils/storage.py
+++ Project-1/app/utils/storage.py
@@ -3,15 +3,21 @@ import shutil
 import uuid
 
 UPLOAD_DIR = Path("app/uploads")
-UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
 
-def save_file(upload_file):
+ORIGINAL_DIR = UPLOAD_DIR / "original"
+PROCESSED_DIR = UPLOAD_DIR / "processed"
+
+ORIGINAL_DIR.mkdir(parents=True, exist_ok=True)
+PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def save_original_file(upload_file):
 
     extension = Path(upload_file.filename).suffix
 
     unique_filename = f"{uuid.uuid4()}{extension}"
 
-    file_path = UPLOAD_DIR / unique_filename
+    file_path = ORIGINAL_DIR / unique_filename
 
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(upload_file.file, buffer)
