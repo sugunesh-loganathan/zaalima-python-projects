e4af15a2a8824c5e1323b68f14f39ebe0b8f615b
Anand Bhagat <ab471448@gmail.com>
2026-07-03 08:46:08 +0530

    Add .env.example and update backend configuration

--- Project-1/app/core/config.py
+++ Project-1/app/core/config.py
@@ -7,3 +7,16 @@ AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = os.getenv("AWS_REGION")
 AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
+CELERY_BROKER_URL = os.getenv(
+    "CELERY_BROKER_URL",
+    "pyamqp://guest:guest@localhost//"
+)
+
+CELERY_RESULT_BACKEND = os.getenv(
+    "CELERY_RESULT_BACKEND",
+    "rpc://"
+)
