6c8e1a0d9aa0515cb930779e8439dac3c9175b27
Anand Bhagat <ab471448@gmail.com>
2026-08-02 22:19:33 +0530

    eip cludwatch

---
+++ Project-2/aws/cloudwatch.py
@@ -0,0 +1,70 @@
+from datetime import datetime, timedelta
+
+from botocore.exceptions import ClientError
+
+from aws.client_factory import AWSClientFactory
+from aws.exceptions import AWSAuthenticationError
+from utils.logger import logger
+
+
+class CloudWatchService:
+    """
+    Service class for CloudWatch metrics.
+    """
+
+    def __init__(self, client_factory: AWSClientFactory):
+        self.client = client_factory.get_client("cloudwatch")
+
+    def get_cpu_utilization(self, instance_id):
+
+        try:
+
+            end_time = datetime.utcnow()
+
+            start_time = end_time - timedelta(days=14)
+
+            response = self.client.get_metric_statistics(
+
+                Namespace="AWS/EC2",
+
+                MetricName="CPUUtilization",
+
+                Dimensions=[
+                    {
+                        "Name": "InstanceId",
+                        "Value": instance_id
+                    }
+                ],
+
+                StartTime=start_time,
+
+                EndTime=end_time,
+
+                Period=86400,
+
+                Statistics=["Average"]
+
+            )
+
+            logger.info(f"CPU metrics fetched for {instance_id}")
+
+            return response
+
+        except ClientError as e:
+
+            logger.error(f"CloudWatch Error: {e}")
+
+            raise AWSAuthenticationError(str(e))
+
+    def get_average_cpu(self, instance_id):
+
+        response = self.get_cpu_utilization(instance_id)
+
+        datapoints = response["Datapoints"]
+
+        if not datapoints:
+            return 0.0
+
+        total = sum(point["Average"] for point in datapoints)
+
+        return round(total / len(datapoints), 2)
--- Project-2/aws/test.py
+++ Project-2/aws/test.py
@@ -1,6 +1,100 @@
+#EC2 SERVICE
+
+
+# from aws.auth import AWSAuth
+# from aws.client_factory import AWSClientFactory
+# from aws.ec2 import EC2Service
+
+
+# auth = AWSAuth(
+#     profile_name="default",
+#     region_name="ap-south-1"
+# )
+
+# factory = AWSClientFactory(auth)
+
+# ec2 = EC2Service(factory)
+
+# instances = ec2.list_instances()
+
+# for instance in instances:
+#     print(instance)
+
+
+#EBS SERVICE
+
+# from aws.auth import AWSAuth
+# from aws.client_factory import AWSClientFactory
+# from aws.ebs import EBSService
+
+# auth = AWSAuth(
+#     profile_name="default",
+#     region_name="ap-south-1"
+# )
+
+# factory = AWSClientFactory(auth)
+# ebs = EBSService(factory)
+
+# # API sirf ek baar call hogi
+# volumes = ebs.list_volumes()
+
+# if not volumes:
+#     print("No EBS volumes found.")
+# else:
+#     print(f"Total Volumes: {len(volumes)}")
+#     for volume in volumes:
+#         print(volume)
+
+# print("\nUnattached Volumes:")
+
+# # Pehle se fetched data pass kar rahe hain
+# unused = ebs.get_unattached_volumes(volumes)
+
+# if not unused:
+#     print("No unattached volumes found.")
+# else:
+#     for volume in unused:
+#         print(volume)
+
+#EIP(ELASTIC IP)
+
+# from aws.auth import AWSAuth
+# from aws.client_factory import AWSClientFactory
+# from aws.eip import ElasticIPService
+
+# auth = AWSAuth(
+#     profile_name="default",
+#     region_name="ap-south-1"
+# )
+
+# factory = AWSClientFactory(auth)
+
+# eip = ElasticIPService(factory)
+
+# addresses = eip.list_addresses()
+
+# if not addresses:
+#     print("No Elastic IPs found.")
+# else:
+#     print(f"Total Elastic IPs: {len(addresses)}")
+#     for address in addresses:
+#         print(address)
+
+# print("\nUnassociated Elastic IPs:")
+
+# unused = eip.get_unassociated_addresses(addresses)
+
+# if not unused:
+#     print("No unassociated Elastic IPs found.")
+# else:
+#     for address in unused:
+#         print(address)
+
+# CLUDWATCH
 from aws.auth import AWSAuth
 from aws.client_factory import AWSClientFactory
-from aws.ebs import EBSService
+from aws.cloudwatch import CloudWatchService
+from aws.ec2 import EC2Service
 
 auth = AWSAuth(
     profile_name="default",
@@ -8,25 +102,21 @@ auth = AWSAuth(
 )
 
 factory = AWSClientFactory(auth)
-ebs = EBSService(factory)
 
-# API sirf ek baar call hogi
-volumes = ebs.list_volumes()
+ec2 = EC2Service(factory)
 
-if not volumes:
-    print("No EBS volumes found.")
+instances = ec2.list_instances()
+
+if not instances:
+    print("No EC2 instances found.")
 else:
-    print(f"Total Volumes: {len(volumes)}")
-    for volume in volumes:
-        print(volume)
 
-print("\nUnattached Volumes:")
+    cloudwatch = CloudWatchService(factory)
 
-# Pehle se fetched data pass kar rahe hain
-unused = ebs.get_unattached_volumes(volumes)
+    for instance in instances:
 
-if not unused:
-    print("No unattached volumes found.")
-else:
-    for volume in unused:
-        print(volume)
+        cpu = cloudwatch.get_average_cpu(
+            instance["InstanceId"]
+        )
+
+        print(instance["InstanceId"], cpu)
